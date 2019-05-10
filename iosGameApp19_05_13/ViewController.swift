280555e00dea8158963f2cf3189c6441cb4ce39c
hsumax0216 <b2626235@gmail.com>
2019-05-10 07:32:25 -0700

    Initial Commit

---
+++ iosGameApp19_05_13/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iosGameApp19_05_13
+//
+//  Created by VMwareMojave on 2019/5/10.
+//  Copyright © 2019 hsumax0216. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
